2fde2e3eb7d3a038b2c2638fe09604cdb6b81adb
KimJisu <wearehappy23@gmail.com>
2024-02-27 18:50:12 +0900

    Upload

--- Source/SelectUnitsSystem/SelectUnitsSystem.Build.cs
+++ Source/SelectUnitsSystem/SelectUnitsSystem.Build.cs
@@ -17,47 +17,5 @@ public class SelectUnitsSystem : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" }); 		
 		PublicIncludePaths.AddRange(new string[] { "SelectUnitsSystem/Public" });		
 		PrivateIncludePaths.AddRange(new string[] { "SelectUnitsSystem/Private" });
-
-  //      PublicIncludePaths.AddRange(
-		//	new string[] {
-		//		// ... add public include paths required here ...
-		//	}
-		//	);
-				
-		
-		//PrivateIncludePaths.AddRange(
-		//	new string[] {
-		//		// ... add other private include paths required here ...
-		//	}
-		//	);
-			
-		
-		//PublicDependencyModuleNames.AddRange(
-		//	new string[]
-		//	{
-		//		"Core",
-		//		// ... add other public dependencies that you statically link with here ...
-		//	}
-		//	);
-			
-		
-		//PrivateDependencyModuleNames.AddRange(
-		//	new string[]
-		//	{
-		//		"CoreUObject",
-		//		"Engine",
-		//		"Slate",
-		//		"SlateCore",
-		//		// ... add private dependencies that you statically link with here ...	
-		//	}
-		//	);
-		
-		
-		//DynamicallyLoadedModuleNames.AddRange(
-		//	new string[]
-		//	{
-		//		// ... add any modules that your module loads dynamically here ...
-		//	}
-		//	);
 	}
 }
